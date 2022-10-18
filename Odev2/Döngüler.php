afd02d37420fe74978a4aec5a2f4db2fed7b76ad
Umutcan <umutcanguller_26@hotmail.com>
2022-10-18 20:51:40 +0300

    Create Döngüler.php

---
+++ Odev2/Döngüler.php
@@ -0,0 +1,62 @@
+<?php
+
+$i = 0;
+$a = 0;
+$c =0;
+$u =0;
+$sehirler = array(
+	"Balıkesir",
+	"Eskişehir",
+	"Kütahya",
+	"Ankara",
+	"İstanbul",
+);
+
+while($i<count($sehirler)-1){
+	print_r($sehirler[$a]);
+	echo "<br />";
+	$i++;
+	$a++;
+}
+echo "<br />";
+
+for($b=0; $b<count($sehirler)-1; $b++)
+{
+	print_r($sehirler[$c]);
+	echo "<br />";
+	$c++;
+	
+	
+}
+	echo "<br />";
+do{
+	
+	print_r($sehirler);
+	
+}while($i<count($sehirler)-1);
+
+foreach($sehirler as $ss){
+	
+	echo $ss."<br />";
+	
+}
+echo "<br />";
+
+for($f = 0;$f<count($sehirler)-1; $f++){
+	
+	if($f == 2){
+		
+		break;
+	}
+	echo "<br />";
+	print_r($sehirler[$u]);
+	$u++;
+}
+
+
+
+
+
+
+
+?>
